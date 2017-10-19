4365ac54de236fb765c5d373bbd31a371908b2d2
Zoran Miladinovic <zoran.miladinovic@endava.com>
2017-10-19 16:54:44 +0200

    Created New Track Activity

--- app/src/main/java/com/androidbootcamp/zoranmiladinovic/endava/locationtracker/MyTracksActivity.java
+++ app/src/main/java/com/androidbootcamp/zoranmiladinovic/endava/locationtracker/MyTracksActivity.java
@@ -1,13 +1,19 @@
 package com.androidbootcamp.zoranmiladinovic.endava.locationtracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MyTracksActivity extends AppCompatActivity {
+public class MyTracksActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_tracks);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, NewTrackActivity.class));
     }
 }
---
+++ app/src/main/java/com/androidbootcamp/zoranmiladinovic/endava/locationtracker/NewTrackActivity.java
@@ -0,0 +1,13 @@
+package com.androidbootcamp.zoranmiladinovic.endava.locationtracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewTrackActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_track);
+    }
+}
